38fda31a17f7ea9bc74afaffc975f8d0f0411dfc
guillaumeag <guillaumeag@users.noreply.github.com>
2023-06-04 15:44:18 -0400

    Refactored and update comments

--- Assets/Scripts/Obstacles/Asteroid.cs
+++ Assets/Scripts/Obstacles/Asteroid.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// INHERITANCE
+/// Represent an asteroid as player's obstacle (INHERITANCE)
 /// </summary>
 public class Asteroid : Obstacle
 {
@@ -16,38 +16,62 @@ public class Asteroid : Obstacle
     private float _minSize = 1.5f;
     private float _maxSize = 3f;
 
+    /// <summary>
+    /// Generate a random asteroid at start
+    /// </summary>
     private void Start()
     {
         // ABSTRACTION
-        // Create random asteroid
-        SetRandomSize();
-        SetRandomMoveSpeed();
+        SetRandomSizeAndSpeed();
     }
 
+    /// <summary>
+    /// Move asteroid at every frame
+    /// </summary>
     private void Update()
     {
         Move();
     }
 
-    private void SetRandomSize()
-    {
-        float randomSize = Random.Range(_minSize, _maxSize);
-
-        transform.localScale = new Vector3(randomSize, randomSize, randomSize);
-    }
-
-    private void SetRandomMoveSpeed()
-    {
-        float randomMoveSpeed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
-        MoveSpeed = randomMoveSpeed;
-    }
-
     /// <summary>
-    /// POLYMORPHISM
+    /// Move asteroid in direction of the bottom of the screen (POLYMORPHISM)
     /// </summary>
     public override void Move()
     {
         float moveDirectionY = _moveDirection * Time.deltaTime * MoveSpeed;
         transform.Translate(transform.position.x, moveDirectionY, transform.position.z);
+    }
+
+    /// <summary>
+    /// Apply random size and movement speed to the asteroid
+    /// </summary>
+    private void SetRandomSizeAndSpeed()
+    {
+        // Set random size (ABSTRACTION)
+        SetLocalScale(RandomScale(_minSize, _maxSize));
+
+        // Set random movement speed
+        MoveSpeed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
+    }
+
+    /// <summary>
+    /// Set the local scale property to change the size of the asteroid
+    /// </summary>
+    /// <param name="scale"></param>
+    private void SetLocalScale(Vector3 scale)
+    {
+        transform.localScale = scale;
+    }
+
+    /// <summary>
+    /// Returns a random scale (<c>Vector3</c>) within [minScale..MaxScale] (range is inclusive).
+    /// </summary>
+    /// <param name="minScale"></param>
+    /// <param name="maxScale"></param>
+    /// <returns>A <c>Vector3</c> representing new the scale</returns>    
+    private Vector3 RandomScale(float minScale, float maxScale)
+    {
+        float randomScaleValue = Random.Range(minScale, maxScale);
+        return new Vector3(randomScaleValue, randomScaleValue, randomScaleValue);
     }
 }
--- Assets/Scripts/Obstacles/Obstacle.cs
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Base class of all obstacles
+/// </summary>
 public class Obstacle : MonoBehaviour
 {
     // Movement
     protected float _moveSpeed = 3.5f;
 
+    /// <summary>
+    /// POLYMORPHISM
+    /// </summary>
     public virtual void Move() { }
 
     /// <summary>
