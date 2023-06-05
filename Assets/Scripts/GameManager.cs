9fa43468e80ec1ff5667c5278c3ce5e4ba0e5d82
guillaumeag <guillaumeag@users.noreply.github.com>
2023-06-05 11:32:13 -0400

    Add sound when player is destroyed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Game Objects")]
+    [SerializeField] private GameObject _player;
+
+    [Header("Sounds")]
+    [SerializeField] private AudioClip _gameOverAudioClip;
+
+    private bool _isGameActive;
+
+    private AudioSource _audioSource;
+
     private void Start()
     {
-        
+        _audioSource = GetComponent<AudioSource>();
+        _isGameActive = true;
+    }
+
+    private void Update()
+    {
+        if (_isGameActive && _player == null)
+        {
+            _audioSource.PlayOneShot(_gameOverAudioClip);
+            _isGameActive = false;
+        }
     }
 }
